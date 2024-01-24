6aa7a93cc17dd3389418f870d39c1aea40cd048d
Gosia Brozek <gosikbrozek1997@gmail.com>
2024-01-24 19:57:49 +0100

    Connect with database and console log messages from appwrite

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,10 +1,13 @@
-import { Client } from 'appwrite';
+import { Client, Databases } from 'appwrite';
+
+export const PROJECT_ID = '65b1472679f86fffd08e'
+export const DATABASE_ID = '65b156248d8cb81e8c48'
+export const COLLECTION_ID = '65b1563e92d875c82b7e'
 
 const client = new Client()
-
-client
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject('65b1472679f86fffd08e')
 
+export const databases = new Databases(client);
 
 export default client
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,5 +1,15 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { COLLECTION_ID, DATABASE_ID, databases } from "../appwriteConfig"
 
 export default function Room() {
+  useEffect(() => {
+    getMessages()
+  }, [])
+
+  const getMessages = async () => {
+    const messages = await databases.listDocuments(DATABASE_ID, COLLECTION_ID)
+    console.log(messages)
+  }
+
   return <div>Room</div>
 }
