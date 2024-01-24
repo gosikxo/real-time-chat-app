fd19e83cba3263947e9e04c3eee6b857dd71c2c6
Gosia Brozek <gosikbrozek1997@gmail.com>
2024-01-24 20:04:02 +0100

    Display messages from appwrite

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { COLLECTION_ID, DATABASE_ID, databases } from "../appwriteConfig"
 
 export default function Room() {
+  const [messages, setMessages] = useState([])
+
   useEffect(() => {
     getMessages()
   }, [])
@@ -9,7 +11,18 @@ export default function Room() {
   const getMessages = async () => {
     const messages = await databases.listDocuments(DATABASE_ID, COLLECTION_ID)
     console.log(messages)
+    setMessages(messages.documents)
   }
 
-  return <div>Room</div>
+  return (
+    <div>
+      <div>
+        {messages.map((messages) => (
+          <div key={messages.$id}>
+            <span>{messages.body}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
