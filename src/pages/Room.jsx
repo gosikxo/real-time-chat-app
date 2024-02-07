d4da00974e2667f64f83bd3249e5184167cc4b93
Gosia Brozek <gosikbrozek1997@gmail.com>
2024-02-07 18:42:45 +0100

    Add form and handle submit message so that it is displayed

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,12 +1,33 @@
 import React, { useEffect, useState } from "react"
 import { COLLECTION_ID, DATABASE_ID, databases } from "../appwriteConfig"
+import { ID } from "appwrite"
 
 export default function Room() {
   const [messages, setMessages] = useState([])
+  const [messageBody, setMessageBody] = useState("")
 
   useEffect(() => {
     getMessages()
   }, [])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    let payload = {
+      body: messageBody,
+    }
+
+    let response = await databases.createDocument(
+      DATABASE_ID,
+      COLLECTION_ID,
+      ID.unique(),
+      payload
+    )
+
+    setMessages((prevState) => [response, ...prevState])
+
+    setMessageBody("")
+  }
 
   const getMessages = async () => {
     const messages = await databases.listDocuments(DATABASE_ID, COLLECTION_ID)
@@ -17,6 +38,20 @@ export default function Room() {
   return (
     <main className="container">
       <div className="room--container">
+        <form onSubmit={handleSubmit} id="message--form">
+          <div>
+            <textarea
+              required
+              maxLength="1000"
+              placeholder="Type your message here"
+              onChange={(e) => setMessageBody(e.target.value)}
+              value={messageBody}
+            />
+          </div>
+          <div className="send-btn--wrapper">
+            <input className="btn btn--secondary" type="submit" value="Send" />
+          </div>
+        </form>
         <div>
           <div>
             {messages.map((messages) => (
